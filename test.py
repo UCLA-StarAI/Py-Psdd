2b0a9eca76de43feb62f1175e3bd1774e17adce8
Honghua Zhang <joshuacnf@outlook.com>
2018-10-12 00:01:44 -0700

    minor modification

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from util import util
 from util import data
 
-if __name__ == '__main__':    
-    sdd = util.sdd_from_file('./examples/big-swap.sdd', './examples/big-swap.vtree')    
+if __name__ == '__main__':
+    sdd = util.sdd_from_file('./examples/big-swap.sdd', './examples/big-swap.vtree')
     print(sdd.base)
-    psdd = util.sdd_to_psdd(sdd=sdd)    
+    psdd = util.sdd_to_psdd(sdd=sdd)
     data_set = data.DataSet(train_data_file="./examples/data.txt")
     psdd.data = data_set.train
-    psdd.calculate_parameter()    
+    psdd.calculate_parameter()
     print(psdd.base)
-    
