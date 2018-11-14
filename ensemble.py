e0a99b8e7959d507241b550dad979ae0beff9df3
Honghua Zhang <joshuacnf@outlook.com>
2018-11-13 19:21:49 -0800

    completed soft-em with parameter learning

--- ensemble.py
+++ ensemble.py
@@ -5,6 +5,7 @@ from util import data
 import random
 import sys
 import os
+import math
 
 task_name = ''
 train_data_file = ''
@@ -15,6 +16,8 @@ cnf_file = ''
 sdd_file = ''
 psdd0_file = ''
 psdd1_file = ''
+label0_file = ''
+label1_file = ''
 
 def init():
     if len(sys.argv) < 2:
@@ -30,6 +33,8 @@ def init():
     global sdd_file
     global psdd0_file
     global psdd1_file
+    global label0_file
+    global label1_file
 
     task_name = sys.argv[1]
     train_data_file = './workspace/{}.train'.format(task_name)
@@ -40,9 +45,12 @@ def init():
     sdd_file = './workspace/{}.sdd'.format(task_name)
     psdd0_file = './workspace/{}.psdd.0'.format(task_name)
     psdd1_file = './workspace/{}.psdd.1'.format(task_name)
+    label0_file = './workspace/{}.label.0'.format(task_name)
+    label1_file = './workspace/{}.label.1'.format(task_name)
 
 def preprocess():
     # generate data set
+    print('generating data set...')
     n, m = None, None
     with open('./workspace/{}.label_matrix'.format(task_name), 'r') as f, \
          open(train_data_file, 'w') as t, \
@@ -65,32 +73,34 @@ def preprocess():
             else:
                 v.write(s)
             o.write(s)
-            
-    exit(0)
+
     # learn_vtree
-    # os.system('java -jar ./lib/psdd.jar learnVtree -d ./workspace/{} -o ./workspace/ -e'.format(train_data_file))
-    # os.system('mv ./workspace/.vtree ./workspace/{}'.format(vtree_file))
+    print('learning vtree from data...')
+    os.system('java -jar ./lib/psdd.jar learnVtree -d {} -o ./workspace/'.format(train_data_file))    
+    os.system('mv ./workspace/.vtree {}'.format(vtree_file))
 
     # gen_cnf
+    print('generating cnf...')
     with open('./workspace/{}.cnf'.format(task_name), 'w') as c:
         c.write('p cnf {} {}\n'.format(m * 2, m))
         for i in range(1, m + 1):
             c.write('{} {} 0\n'.format(-(2 * i - 1), 2 * i))
 
     # compile_sdd
-    # os.system('./lib/sdd-linux -c {} -v {} -R {} -r 0'.format(cnf_file, vtree_file, sdd_file))
+    print('compiling sdd...')
+    os.system('./lib/sdd-linux -c {} -v {} -R {} -r 0'.format(cnf_file, vtree_file, sdd_file))
 
     # generate initial psdds
+    print('generating psdds...')
     vtree = util.vtree_from_file(vtree_file)
-    print(vtree.left.idx, vtree.right.idx)
     sdd = util.sdd_from_file(sdd_file, vtree_file)
-    print(len(sdd._elements))
-    sdd = algo.normalize(sdd, vtree)
-    print(len(sdd._elements))
-    exit(0)
+    cache_lit, cache_idx = {}, {}
+    sdd = algo.normalize(sdd, vtree, cache_lit, cache_idx)
     sdd._node_count = algo.re_index(sdd)
-    util.psdd_to_file(psdd0, psdd0_file)
+    psdd0 = util.sdd_to_psdd(sdd)
     psdd1 = util.sdd_to_psdd(sdd)
+    util.psdd_to_file(psdd0, psdd0_file)
+    util.psdd_to_file(psdd1, psdd1_file)
     data_set = data.DataSet(train_data_file=train_data_file, valid_data_file=valid_data_file)
 
     return psdd0, psdd1, data_set
@@ -120,9 +130,10 @@ def EM(psdd0, psdd1, data_set):
     algo.compute_parameter(psdd0)
     algo.compute_parameter(psdd1)
 
-    for i in range(1000):
+    count = 0
+    for i in range(2):
         # structure learning
-        for j in range(50):
+        for j in range(100):
             W0, W1 = 0.0, 0.0
             for asgn, w in data0.items():
                 W0 += w
@@ -149,9 +160,47 @@ def EM(psdd0, psdd1, data_set):
             train_ll = compute_ensemble_log_likelihood(psdd0, p0, psdd1, p1, data_set.train)
             valid_ll = compute_ensemble_log_likelihood(psdd0, p0, psdd1, p1, data_set.valid)
 
-            print('{} {}, train_ll: {} valid_ll: {}', train_ll, valid_ll)
+            print('{} {}, train_ll: {} valid_ll: {}'.format(i, j, train_ll, valid_ll))
+            with open('./workspace/log.txt', 'a') as f:
+                f.write('{} {} {}\n'.format(count, train_ll, valid_ll))
+            count += 1
+
+    W0, W1 = 0.0, 0.0
+    for asgn, w in data0.items():
+        W0 += w
+    for asgn, w in data1.items():
+        W1 += w
+    p0 = W0 / (W0 + W1)
+    p1 = W1 / (W0 + W1)
+    return psdd0, psdd1, p0, p1
+
+def generate_labels(psdd0, psdd1, p0, p1):
+    label0 = []
+    label1 = []
+    with open(sample_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            d = [ (x == '1') for x in line.split(',') ]
+            asgn = (None, )
+            for x in d:
+                asgn = asgn + (x, )
+            q0 = algo.compute_probability(psdd0, asgn)
+            q1 = algo.compute_probability(psdd1, asgn)
+            l0 = (q1 * p1) / (q0 * p0 + q1 * p1)
+            l1 = (q0 * p0) / (q0 * p0 + q1 * p1)
+            label0.append(l0)
+            label1.append(l1)
+    
+    with open(label0_file, 'w') as f:
+        for x in label0:
+            f.write('{}\n'.format(x))
+    
+    with open(label1_file, 'w') as f:
+        for x in label1:
+            f.write('{}\n'.format(x))
 
 if __name__ == '__main__':
     init()
     psdd0, psdd1, data_set = preprocess()
-    EM(psdd0, psdd1, data_set)
+    psdd0, psdd1, p0, p1 = EM(psdd0, psdd1, data_set)
+    generate_labels(psdd0, psdd1, p0, p1)
